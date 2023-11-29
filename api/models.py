6447658914602bab68b3dff3485d80c4a923ccb3
Rohit Thakre <thake785@gmail.com>
2023-11-29 15:29:30 +0530

    some minor changes.

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Vendor(models.Model):
     on_time_delivery_rate = models.FloatField(default=0)
     quality_rating_avg = models.FloatField(default=0)
     average_response_time = models.FloatField(default=0)
-    fulfillment_rate = models.FloatField(default=0)  
+    fulfillment_rate = models.FloatField(default=0) 
 
     def __str__(self) -> str:
         return self.name
